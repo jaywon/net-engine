0962bf675dfba3003016d1c6c75558e1cc354e5c
jaywon <jason@sudokrew.com>
2018-01-28 13:48:23 -1000

    :globe_with_meridians: Add copy in English/Spanish for firewall rule violation

--- copy.py
+++ copy.py
@@ -2,5 +2,9 @@ copy_text = {
     "SELECT_TOOL": {
         "en": "Select your tool",
         "es": "Seleccione su herramienta"
+    },
+    "FIREWALL_RULE_VIOLATION": {
+        "en": "FIREWALL RULE VIOLATED...YOU HAVE BEEN DETECTED!!!",
+        "es": "REGLA DE CORTAFUEGOS VIOLADA... HA SIDO DETECTADO!!!"
     }
 }
